f9ce4f4e779a82dd1b0e6223eeb35281ba923cfe
Your Name <you@example.com>
2020-07-10 15:03:28 +0100

    Collections

---
+++ Intro/collections.py
@@ -0,0 +1,59 @@
+# shopping_list = ["eggs", "cheese", "sausages", "bread", 5, 2.5, True, [1, 2, 3]]
+# print(shopping_list)
+# print(type(shopping_list))
+# print(shopping_list[-1])
+#
+# shopping_list.append("mushrooms")
+# print(shopping_list)
+# shopping_list.remove(True)
+# print(shopping_list)
+# pop_list = shopping_list.pop()
+# print(pop_list)
+# print(shopping_list.pop(0))
+#
+# colours = ("blue", "purple", "orange")
+# print(colours)
+# print(colours.count("blue"))
+# print(colours.index("blue"))
+#
+# my_dict = {
+#     "Fisrt Name": "Anthony",
+#     "Last Name": "McHugh",
+#     "Age": 26,
+#     "That is": "Old"
+# }
+# print(my_dict)
+#
+# print(my_dict["Fisrt Name"])
+# my_dict["That is"] = "Young"
+# print(my_dict)
+# my_dict["New"] = 21
+# print(my_dict)
+# del my_dict["New"]
+# print(my_dict)
+# print(my_dict.keys())
+# print(my_dict.values())
+
+# data_14 = {
+#     "Banter": "Juxhen",
+#     "Robot": "Charlie",
+#     "Eco Warrior": "Katie",
+#     "Sophistication": "Charlotte",
+#     "Big Man": "Joe",
+#     "Boss": "David",
+#     "Feels": "Danny",
+#     "Circle": ["Charlie", "Danny", "Juxhen", "Joe"]
+# }
+# print(data_14)
+# print(data_14["Circle"][1])
+
+# parts = {"wheels", "doors", "clutch"}
+# print(parts)
+# print(type(parts))
+# #sets are an unordered list
+# parts.add("roof")
+# parts.remove("wheels")
+# print(parts)
+#
+# fs = frozenset([1, 2, 3, 4])
+# print(fs)
