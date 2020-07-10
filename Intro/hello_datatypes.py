a6527dc67806654517022fbb3069fff4862f681c
Your Name <you@example.com>
2020-07-10 12:36:36 +0100

    Intro to Python

---
+++ Intro/hello_datatypes.py
@@ -0,0 +1,55 @@
+# a = 3
+# b = 1
+#
+# print(a != b)
+# print(a == b)
+
+# a = 1
+# b = 1.0
+# c = 2+3j
+#
+#
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# hw = "Hello World!"
+# print(hw[-5])
+# print(len(hw))
+# 0 1 2 3 4 5 6 7 8 9 10 11
+# H e l l o   W o r l d  !
+
+# space = "         I LOVE SPACE!         "
+# print(space)
+# print(space.strip().lower())
+# print(space.lstrip())
+# print(space.rstrip())
+# print(space.count(" "))
+# print(space.count("V"))
+# print(space.lower())
+
+# a = "hi"
+# b = "yo"
+# c = 5
+#
+# print(a + ", " + b + ", " + str(c))
+# print(f"{a}, {b}, {c}")
+
+# hw = "HelloWorld1!"
+# # .isalpha shows True if string ONLY contains alphabetical values
+# print(hw.isalpha())
+# print(hw.isalnum())
+# print(hw.lower().islower())
+# print(hw.upper().isupper())
+# print(hw.endswith("!"))
+# print(hw.startswith("H"))
+
+x = 0
+y = 10
+print(bool(x))
+print(bool(y))
+
+n = None
+print(bool(n))
+print(n != False)
+print(n is None)
---
+++ Intro/hello_variables.py
@@ -0,0 +1,20 @@
+# a = 5
+# b = 12
+# c = (2, 3, 4)
+#
+# print(type(c))
+
+
+
+name = str(input("What is your name?\n"))
+print("Hello, " + name + "!")
+dob = str(input("What is your D.O.B?\n"))
+age = int(input("Then how old are you?\n"))
+print(f"You're {age}?")
+print("Wow! You're secretly old!")
+siblings = int(input("How many siblings do you have?\n"))
+print(f"{siblings}! Nice!")
+decimal = float(input("Also, what's your favourite decimal number?\n"))
+print("Interesting, " + decimal + ", what about animals?")
+animal = str(input("What about animals?\n"))
+print(f"I like {animal} too! Well I think i know you much better now!")
