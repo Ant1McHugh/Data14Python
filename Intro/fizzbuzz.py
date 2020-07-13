dd79b003325a479a9b5bfeab0c02d933c7bbad77
Your Name <you@example.com>
2020-07-13 12:31:55 +0100

    fizzbuzz functions

--- Intro/fizzbuzz.py
+++ Intro/fizzbuzz.py
@@ -5,36 +5,39 @@
 # if divisible by 5, say buzz
 # if divisible by both, say fizzbuzz
 
+def numeric_check(value):
+    while not value.isnumeric():
+        value = input("Please enter a valid number\n")
+    return value
+
+
 fizz_input = input("Please choose an alternative to Fizz\n")
 
 buzz_input = input("Please choose an alternative to Buzz\n")
 
 start_point = input("Choose a starting number\n")
 
-while not start_point.isnumeric():
-    print("Please enter a valid number")
-    start_point = input("Choose a starting number\n")
+start_point = int(numeric_check(start_point))
 
 end_point = input("Choose where to end the cycle\n")
 
-while not end_point.isnumeric():
-    print("Please enter a valid number")
-    end_point = input("Choose where to end the cycle\n")
+end_point = int(numeric_check(end_point))
 
 while end_point <= start_point:
     print("Your end point must be greater than the start point, please try again!")
     end_point = input("Choose where to end the cycle\n")
 
-while not end_point.isnumeric():
-    print("Please enter a valid number")
-    end_point = input("Choose where to end the cycle\n")
 
-for number in range(int(start_point), int(end_point)):
-    if number % 3 == 0 and number % 5 == 0:
-        print(f"{fizz_input}{buzz_input}")
-    elif number % 3 == 0:
-        print(f"{fizz_input}")
-    elif number % 5 == 0:
-        print(f"{buzz_input}")
-    else:
-        print(number)
+def fizzbuzz_game(start, end):
+    for number in range(int(start), int(end)):
+        if number % 3 == 0 and number % 5 == 0:
+            print(f"{fizz_input}{buzz_input}")
+        elif number % 3 == 0:
+            print(f"{fizz_input}")
+        elif number % 5 == 0:
+            print(f"{buzz_input}")
+        else:
+            print(number)
+
+
+print(fizzbuzz_game(start_point, end_point))
